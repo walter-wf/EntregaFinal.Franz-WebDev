e18f09fd80234b78bac7ba0d70ad3741c925437a
walter-wf <walter.franz@gmail.com>
2023-09-27 18:12:43 -0300

    se agrega responsive, mejoras en el codigo y un poco de scss

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function () {
+/* document.addEventListener("DOMContentLoaded", function () {
   // Función para cambiar entre el modo día y noche
   function toggleDayNightMode() {
     const bodyElement = document.querySelector("body");
@@ -8,4 +8,13 @@ document.addEventListener("DOMContentLoaded", function () {
   // Obtener el botón y agregar el evento click
   const toggleButton = document.getElementById("mode-toggle");
   toggleButton.addEventListener("click", toggleDayNightMode);
+}); */
+
+// Obtener elementos del DOM
+const hamburgerBtn = document.getElementById('hamburger-btn');
+const navbarNav = document.getElementById('navbarNav');
+
+// Agregar evento click al botón hamburguesa
+hamburgerBtn.addEventListener('click', () => {
+    navbarNav.classList.toggle('show');
 });
